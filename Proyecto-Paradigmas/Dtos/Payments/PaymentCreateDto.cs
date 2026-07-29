057ef3dc115590cebaddbd7336e7759f9c860900
Diego Maldonado <dmaldonadochinchilla561@gmail.com>
2026-07-28 20:45:12 -0600

    feature: ModulePaymentPaypal

---
+++ Proyecto-Paradigmas/Dtos/Payments/PaymentCreateDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Proyecto_Paradigmas.Dtos.Payments
+{
+    public class PaymentCreateDto
+    {
+        public int ReservationId { get; set; }
+        public decimal TotalAmount { get; set; }
+    }
+}
